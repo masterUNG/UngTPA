eba0e3a03357842bc6d4db44d4383d9c52a91adb
masterUNG <phrombutr@gmail.com>
2019-07-09 15:58:23 +0700

    Fonts Success

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
 
-
 class Authen extends StatefulWidget {
   @override
   _AuthenState createState() => _AuthenState();
-  
 }
 
 class _AuthenState extends State<Authen> {
+  // Explicit
+
+  // Method
+  Widget showLogo() {
+    return Container(
+      width: 180.0,
+      height: 180.0,
+      child: Image.asset(
+        'images/logo.png',
+      ),
+    );
+  }
+
+  Widget showText() {
+    return Text(
+      'Ung TPA',
+      style: TextStyle(
+        fontSize: 40.0,
+        color: Colors.orange[900],
+        fontWeight: FontWeight.bold,
+        fontFamily: 'MountainsofChristmas',
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Authen'),);
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 60.0),
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: <Widget>[
+            showLogo(),
+            showText(),
+          ],
+        ),
+      ),
+    );
   }
 }
