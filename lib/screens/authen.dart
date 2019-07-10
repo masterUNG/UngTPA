8661194e830c0aa05f94cbdf999c46fb09275d2c
masterUNG <phrombutr@gmail.com>
2019-07-10 11:22:39 +0700

    Design UI Authen Success

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix0;
 
 class Authen extends StatefulWidget {
   @override
@@ -57,11 +58,60 @@ class _AuthenState extends State<Authen> {
     );
   }
 
+  Widget signInButton() {
+    return RaisedButton(
+      color: Colors.orange[900],
+      child: Text(
+        'Sign In',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget signUpButton() {
+    return RaisedButton(
+      color: Colors.orange[200],
+      child: Text('Sign Up'),
+      onPressed: () {},
+    );
+  }
+
+  Widget showButton() {
+    return Container(
+      width: 250.0,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: signInButton(),
+          ),
+          mySizeBox(),
+          Expanded(
+            child: signUpButton(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget mySizeBox() {
+    return SizedBox(
+      width: 8.0,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: [Colors.white, Colors.orange[900]],
+            radius: 2.0,
+            center: Alignment.topCenter,
+          ),
+        ),
         padding: EdgeInsets.only(top: 60.0),
         alignment: Alignment.topCenter,
         child: Column(
@@ -70,6 +120,7 @@ class _AuthenState extends State<Authen> {
             showText(),
             emailText(),
             passwordText(),
+            showButton(),
           ],
         ),
       ),
